d5dbfbeeb646563149e9b556c2b7e2b9f029b0b4
Yinon Elbaz <yinone@intramop.local>
2019-10-28 16:50:36 +0200

    the first Class of app

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import LoginScreen from './view/LoginScreen';
+
+// const loginScreen = new LoginScreen('.main');
+
+window.loginScreen = new LoginScreen('#main');
---
+++ src/js/view/LoginScreen.js
@@ -0,0 +1,62 @@
+class LoginScreen {
+
+    constructor(selector) {
+
+       this.selector = selector;  
+       this.template = '';
+       this.strings = ['להפקדת שיק', 'לפדיון קופה', 'הפקדת פרס כספי'];
+
+    }
+
+    accountBalance(num) {
+
+        this.template += `
+        <div class="account-balance">
+            <p>₪ יתרת חשבון<span class="account-balance__num"> ${num} </span></p>
+        </div>
+        `;
+
+    }
+
+    entry(stateNum) {
+
+        this.template += `
+        <div class="entry">
+            ${this.entrySection(stateNum)}
+        </div>
+        `;
+
+    }
+
+    entrySection(stateNum) {
+
+        return this.strings.map((string, i) => {
+
+            return `
+            <div class="entry__section ${i == 1 ? 'entry__section--center' : ''}">
+                <div class="entry__text ${i < stateNum ? 'entry__text--done' : ''}">
+                    <h3>${string}</h3>
+                </div>
+                <div class="entry__btn">
+                    <a href="#">כניסה</a>
+                </div>
+            </div>
+            `
+        }).join('');
+
+    }
+
+    printLoginScreen(stateNum) {
+
+        if (stateNum > 3) stateNum = 3;
+
+            this.accountBalance(500 * stateNum);
+            this.entry(stateNum);
+            document.querySelector(this.selector).innerHTML = this.template;
+            this.template = "";
+
+    }
+
+}
+
+export default LoginScreen;
